34fa41618cfaa6fe120b1e3973b742669f5daf6e
Kittisak Srathong <basbubie.kittisak@gmail.com>
2024-10-15 02:02:12 +0700

    [UPDATE] setup axios for fetch data

--- client/src/utils/axios.tsx
+++ client/src/utils/axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = `${process.env.API_URL}`;
+const BASE_URL = `${process.env.REACT_APP_API_URL}`;
 
 const token = "Bearer ";
 
--- client/src/utils/swrService.tsx
+++ client/src/utils/swrService.tsx
@@ -2,8 +2,10 @@ import axios from "./axios";
 
 export const fetcher = async (url: string) => {
   try {
-    const { data } = await axios.get(url);
-    return data;
+    const {
+      data: { data, message },
+    } = await axios.get(url);
+    return { data, message };
   } catch (error) {
     return Promise.reject(error);
   }
