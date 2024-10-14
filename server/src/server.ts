e1a82e8c25bbb10365d748ca47970d6ce35a666c
Kittisak Srathong <basbubie.kittisak@gmail.com>
2024-10-14 23:06:10 +0700

    [DELETE] unused lib morgan

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import morgan from "morgan";
 import { router as authRouter } from "./routes/auth.route";
 import { connectDatabase } from "./config/database";
 import dotenv from "dotenv";
@@ -11,7 +10,6 @@ const app = express();
 dotenv.config();
 app.use(cors());
 app.use(bodyParser.json({ limit: "2mb" }));
-app.use(morgan("dev"));
 
 connectDatabase();
 
